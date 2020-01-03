4f47d679b9e23119d9beacd06f59ab0493a023d4
devcolorcoded <40136823+devcolorcoded@users.noreply.github.com>
2020-01-03 17:23:34 -0500

    iOS make writeSomething button
    Create a custom view for making a post, remove existing text view and button and replace with custom view

---
+++ frontend/ios/Hive/Hive/MakePostView.swift
@@ -0,0 +1,62 @@
+//
+//  MakePostView.swift
+//  Hive
+//
+//  Created by Chuck Onwuzuruike on 1/3/20.
+//  Copyright © 2020 Chuck Onwuzuruike. All rights reserved.
+//
+
+import UIKit
+
+protocol MakePostViewDelegate: class {
+    func makePost(text: String)
+}
+
+class MakePostView: UIView {
+
+    @IBOutlet weak var postTextView: UITextView!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var postButton: UIButton!
+    
+    private(set) var delegate: MakePostViewDelegate?
+    private(set) var parent: UIView?
+    
+    public func configure(parent: UIView, delegate: MakePostViewDelegate) {
+        self.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        self.parent = parent
+        self.delegate = delegate
+    }
+    
+    public func show() {
+        self.isUserInteractionEnabled = true
+        self.parent?.addSubview(self)
+        self.parent?.bringSubviewToFront(self)
+    }
+    
+    public func clearAndShow() {
+        clearPostText()
+        show()
+    }
+    
+    public func hide() {
+        removeFromSuperview()
+    }
+    
+    public func clearPostText() {
+        DispatchQueue.main.async {
+            self.postTextView.text = ""
+        }
+    }
+    
+    @IBAction func cancelButtonAction(_ sender: UIButton) {
+        hide()
+    }
+    
+    @IBAction func postButtonAction(_ sender: UIButton) {
+        if postTextView.text.isEmpty {
+            return
+        }
+        delegate!.makePost(text: postTextView.text)
+    }
+    
+}
--- frontend/ios/Hive/Hive/ViewController.swift
+++ frontend/ios/Hive/Hive/ViewController.swift
@@ -13,8 +13,8 @@ class ViewController: UIViewController {
     let COMMENTS_SEGUE_IDENTIFIER = "seeCommentsSegue"
     
     @IBOutlet weak var postFeedTable: UITableView!
-    @IBOutlet weak var postTv: UITextView!
-    @IBOutlet weak var postBn: UIButton!
+    @IBOutlet weak var writeSomethingBn: UIButton!
+    private var makePostView: MakePostView!;
     
     private var postFeedManager = PostFeedManager()
     private(set) var client: ServerClient = ServerClient()
@@ -23,40 +23,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.hideKeyboardWhenTapped()
-        setupPostTv()
-        setupPostBn()
+        setupMakePostAlert()
         setupPostFeedView()
-    }
-
-    private func setupPostBn() {
-        postBn.layer.cornerRadius = 10
-        postBn.layer.borderWidth = 1
-        postBn.layer.borderColor = UIColor.black.cgColor
-    }
-    
-    private func setupPostTv() {
-        // Take care of border
-        postTv.layer.cornerRadius = 10.0
-        postTv.layer.borderWidth = 1.0
-        postTv.layer.borderColor = UIColor.black.cgColor
     }
     
     private func setupPostFeedView() {
         postFeedManager.configure(tableView: postFeedTable, delegate: self)
     }
-
-    @IBAction func postBnAction(_ sender: UIButton) {
-        if (postTv.text.isEmpty) {
-            return
-        }
-        DispatchQueue.main.async {
-            self.postTv.isEditable = false
-            self.postTv.isSelectable = false
-            sender.isEnabled = false
-        }
-        client.insertPost(username: self.getTestUser(),
-                          postText: postTv.text, location: self.getTestLocation(),
-                          completion: insertPostCompletion, notes: nil)
+    
+    private func setupMakePostAlert() {
+        makePostView = UIView.loadFromNibNamed(nibNamed: "MakePostView") as? MakePostView
+        makePostView.configure(parent: self.view, delegate: self)
+    }
+    
+    @IBAction func writeSomethingBnAction(_ sender: UIButton) {
+        makePostView.show()
     }
     
     private func insertPostCompletion(response: StatusOr<Response>, notes: [String:Any]?) {
@@ -78,13 +59,9 @@ class ViewController: UIViewController {
         }
         
         self.postFeedManager.pokeNew()
+        makePostView.clearPostText()
         DispatchQueue.main.async {
-            self.postTv.isEditable = true
-            self.postTv.isSelectable = true
-            if (!error) {
-                self.postTv.text = ""
-            }
-            self.postBn.isEnabled = true
+            self.view.isUserInteractionEnabled = true
             self.postFeedManager.reload()
         }
     }
@@ -165,6 +142,18 @@ extension ViewController: PostFeedDelegate {
         client.updatePost(postId: post.postId, username: getTestUser(),
             actionType: actionType, completion: updatePostCompletion,
             notes: ["actionType": actionType, "postId": post.postId])
+    }
+}
+
+extension ViewController: MakePostViewDelegate {
+    func makePost(text: String) {
+        makePostView.hide()
+        DispatchQueue.main.async {
+            self.view.isUserInteractionEnabled = true
+        }
+        client.insertPost(username: self.getTestUser(),
+                          postText: text, location: self.getTestLocation(),
+                          completion: insertPostCompletion, notes: nil)
     }
 }
 
